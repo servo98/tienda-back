f681fbfa3a31b17026f8ae78fe9ab05be86da4c6
Eddd1e <editiend@gmail.com>
2022-07-03 15:02:53 -0500

    fix2

--- api/controllers/pagoController.js
+++ api/controllers/pagoController.js
@@ -23,10 +23,10 @@ const createPago = async (req, res) => {
 };
 
 const getAllPagos = async (req, res) => {
-  if (req.persona.id_rol != 1) {
+  if (req.query.persona.id_rol != 1) {
     req.query.id_persona = req.persona.id;
   }
-  if(req.params.id_estado == "*"){
+  if(req.query.params.id_estado == "*"){
     delete params.query.id_estado;
   }
   if(req.params.id_tipo == "*"){
